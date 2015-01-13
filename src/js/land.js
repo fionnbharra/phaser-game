ba6c2fc6b91d30a3943e39cece3127a55cdf40bc
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-01-13 20:39:46 +0000

    refactor

--- src/js/land.js
+++ src/js/land.js
@@ -1,30 +1,17 @@
 function Land(game) {
   'use strict';
-
-  this.game = game;
-  return this.init();
+  this.map = game.add.tilemap('level1');
+  //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
+  this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
+  this.createLayers(this.map);
+  this.backgroundlayer.resizeWorld();
 }
 
 Land.prototype = {
-  init: function(){
-    this.map = this.game.add.tilemap('level1');
-
-    //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
-    this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
-    //
-    // //create layers
-    //
-    this.backgroundlayer = this.map.createLayer('backgroundLayer');
-    //
-    this.blockedLayer = this.map.createLayer('blockedLayer');
-    //
-    // //collision on blockedLayer
-    //
+  createLayers: function (map) {
+    this.backgroundlayer = map.createLayer('backgroundLayer');
+    this.blockedLayer = map.createLayer('blockedLayer');
     this.map.setCollisionBetween(1, 100000, true, 'blockedLayer');
-
-    //resizes the game world to match the layer dimensions
-    this.backgroundlayer.resizeWorld();
-    return this;
   }
 };
 
