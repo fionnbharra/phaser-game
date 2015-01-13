e2c383420498b623be30d76cb18f9139e7c68e7c
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-01-13 21:12:51 +0000

    whitespace

--- src/js/game.js
+++ src/js/game.js
@@ -25,31 +25,24 @@ Game.prototype = {
     }
     // Reset the players velocity (movement)
     this.player.sprite.body.velocity.x = 0;
-    if (this.cursors.left.isDown)
-    {
+    if (this.cursors.left.isDown) {
       //  Move to the left
       this.player.sprite.body.velocity.x = -150;
-
       this.player.sprite.animations.play('left');
     }
-    else if (this.cursors.right.isDown)
-    {
+    else if (this.cursors.right.isDown) {
       //  Move to the right
       this.player.sprite.body.velocity.x = 150;
-
       this.player.sprite.animations.play('right');
     }
-    else
-    {
+    else {
       //  Stand still
       this.player.sprite.animations.stop();
-
       this.player.sprite.frame = 4;
     }
 
     //  Allow the this.player.sprite to jump if they are touching the ground.
-    if (this.cursors.up.isDown && this.player.sprite.body.blocked.down)
-    {
+    if (this.cursors.up.isDown && this.player.sprite.body.blocked.down) {
       this.player.sprite.body.velocity.y = -350;
     }
   }
